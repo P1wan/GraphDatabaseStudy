34043aaaf8731491798be24379f493b7d4991440
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-05-28 09:19:46 -0300

    Update FITS_TO_CSV.py

--- datasets/FITS_TO_CSV.py
+++ datasets/FITS_TO_CSV.py
@@ -1,4 +1,13 @@
 from astropy.table import Table
+from astropy import units as u
 
-t = Table.read('kepler_dr3_good.fits')
-t.write('matches.csv', format='csv')
+# Define custom units
+u.def_unit('dex', u.dimensionless_unscaled)
+u.def_unit('log(cm.s**-2)', u.dimensionless_unscaled)
+
+# SE NÃO ESTIVER EXECUTANDO DA RAIZ DO PROJETO, COMENTE A LINHA ABAIXO E DESCOMENTE A SEGUINTE
+path = 'datasets/'
+# path = ''
+
+data = Table.read(path + 'kepler_dr3_good.fits', format='fits')
+data.write(path + 'matches.csv', format='csv', overwrite=True)
